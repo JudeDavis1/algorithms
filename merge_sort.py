1b81b3e272ede21b307c813a36fe5b914d91381a
Jude Davis (40191355) <40191355@hcuc.ac.uk>
2022-11-18 12:12:03 +0000

    Initial commit

---
+++ merge_sort.py
@@ -0,0 +1,53 @@
+import random
+
+
+def main():
+    l = list(range(1, 10))
+    random.shuffle(l)
+    merge_sort(l)
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+    
+    # Find midpoint
+    mid = len(arr) // 2
+    L = arr[:mid]
+    R = arr[mid:]
+    print(L)
+    
+    # Sort Left and Right
+    merge_sort(L)
+    merge_sort(R)
+
+    # Sort in-place so that we don't have to return anything.
+
+    i = j = k = 0
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            arr[k] = L[i]
+            i += 1  # Move onto the next item
+        else:
+            arr[k] = R[j]
+            j += 1  # Same here
+        k += 1
+    
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
